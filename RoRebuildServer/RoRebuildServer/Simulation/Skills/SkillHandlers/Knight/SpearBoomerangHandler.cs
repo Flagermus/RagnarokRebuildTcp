b83e5d5fc0aea7db196df28f05be7299333a4613
Flagermus <flagermus0812@gmail.com>
2026-08-03 18:06:10 +0700

    Rework Spear Boomerang

--- RoRebuildServer/RoRebuildServer/Simulation/Skills/SkillHandlers/Knight/SpearBoomerangHandler.cs
+++ RoRebuildServer/RoRebuildServer/Simulation/Skills/SkillHandlers/Knight/SpearBoomerangHandler.cs
@@ -13,15 +13,7 @@ public class SpearBoomerangHandler : SkillHandlerBase
 {
     public override int GetSkillRange(CombatEntity source, int lvl)
     {
-        return lvl switch
-        {
-            1 => 5,
-            2 => 6,
-            3 => 8,
-            4 => 9,
-            5 => 11,
-            _ => 5,
-        };
+        return 13;
     }
 
     public override SkillValidationResult ValidateTarget(CombatEntity source, CombatEntity? target, Position position, int lvl,
@@ -36,16 +28,18 @@ public class SpearBoomerangHandler : SkillHandlerBase
     public override void Process(CombatEntity source, CombatEntity? target, Position position, int lvl,
         bool isIndirect, bool isItemSource)
     {
-        lvl = lvl.Clamp(1, 10);
+        lvl = lvl.Clamp(1, 1);
 
         if (target == null || !target.IsValidTarget(source))
             return;
 
-        var res = source.CalculateCombatResult(target, 1f + lvl * 0.5f, 1, AttackFlags.Physical | AttackFlags.Ranged, CharacterSkill.SpearBoomerang);
+        var res = source.CalculateCombatResult(target, 3.0f, 1, AttackFlags.Physical | AttackFlags.Ranged, CharacterSkill.SpearBoomerang);
         res.AttackMotionTime = 0.2f; //throw motion is half as long as the attack one
         res.Time = Time.ElapsedTimeFloat + 0.3f + source.Character.Position.DistanceTo(target.Character.Position) / ServerConfig.ArrowTravelTime;
-        source.ApplyCooldownForSupportSkillAction();
         source.ExecuteCombatResult(res, false);
+
+        if (source.Character.Type == CharacterType.Player)
+            source.Player.SetSkillSpecificCooldown(CharacterSkill.SpearBoomerang, 3f);
 
         var ch = source.Character;
 
